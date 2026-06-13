8dc7a7aae38aee5db33e83f384836bbcbc4f0476
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-13 23:40:09 +0930

    Refactor config object syntax in main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
 const config = {
-    type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
-    backgroundColor: "#000000",
-
-    physics: {
-        default: "arcade",
-        arcade: { gravity: { y: 0 }, debug: false }
-    },
-
-    scene: [
-        BootScene,
-        GameScene,
-        UIScene
-    ]
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  backgroundColor: '#000000',
+  physics: {
+    default: 'arcade',
+    arcade: { gravity: { y: 0 }, debug: false }
+  },
+  scene: [BootScene, GameScene, UIScene]
 };
 
 new Phaser.Game(config);
