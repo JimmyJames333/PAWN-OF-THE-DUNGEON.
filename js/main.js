84763cfd0e3f717b57d64be5de38dc9f8cad7b1c
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-13 22:30:45 +0930

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    backgroundColor: "#000000",
+
+    physics: {
+        default: "arcade",
+        arcade: { gravity: { y: 0 }, debug: false }
+    },
+
+    scene: [
+        BootScene,
+        GameScene,
+        UIScene
+    ]
+};
+
+new Phaser.Game(config);
