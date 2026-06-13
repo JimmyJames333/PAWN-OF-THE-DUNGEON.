9560999796d5bfbe0065ef5d15b1df604c628e70
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-14 00:00:15 +0930

    Add text display for game start notification

--- js/scenes/GameScene.js
+++ js/scenes/GameScene.js
@@ -5,9 +5,14 @@ class GameScene extends Phaser.Scene {
 
   create() {
     const { width, height } = this.scale;
+
     this.add.text(width / 2, height / 2, 'Game Started!', {
       fontSize: '32px',
-      color: '#00ff00' // ✅ Correct hex code
+      color: '#00ff00'
     }).setOrigin(0.5);
+
+    // Example map generation
+    this.map = new MapGenerator(this);
+    this.map.createMap();
   }
 }
