38bae743a927fcd5f5d8f36f5d3081096cca9ea9
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-13 23:42:57 +0930

    Update BootScene.js

--- js/scenes/BootScene.js
+++ js/scenes/BootScene.js
@@ -1,22 +1,20 @@
+// js/scenes/BootScene.js
 class BootScene extends Phaser.Scene {
   constructor() {
     super('BootScene');
   }
 
   preload() {
-    // Load assets here
     this.load.image('logo', 'assets/sprites/logo.png');
   }
 
   create() {
-    // Simple loading splash
     const { width, height } = this.scale;
     this.add.text(width / 2, height / 2, 'Loading...', {
       fontSize: '32px',
       color: '#ffffff'
     }).setOrigin(0.5);
 
-    // Move to GameScene after short delay
     this.time.delayedCall(1000, () => {
       this.scene.start('GameScene');
     });
