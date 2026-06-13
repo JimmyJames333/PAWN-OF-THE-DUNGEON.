b5859084ed9ba9f2692942cba943509baf7aeec1
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-13 22:39:03 +0930

    Create SynergySystem.js

---
+++ js/systems/SynergySystem.js
@@ -0,0 +1,10 @@
+class SynergySystem {
+    constructor(scene) {
+        this.scene = scene;
+        this.synergies = [];
+    }
+
+    addSynergy(name) {
+        this.synergies.push(name);
+    }
+}
