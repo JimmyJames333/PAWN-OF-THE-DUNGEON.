99f7048af287e5ae941a2670b944d240a995bcb4
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-13 23:56:08 +0930

    Update GameScene.js

--- js/scenes/GameScene.js
+++ js/scenes/GameScene.js
@@ -7,7 +7,7 @@ class GameScene extends Phaser.Scene {
     const { width, height } = this.scale;
     this.add.text(width / 2, height / 2, 'Game Started!', {
       fontSize: '32px',
-      color: '#00ff00'
+      color: '#00ff00' // ✅ Correct hex code
     }).setOrigin(0.5);
   }
 }
