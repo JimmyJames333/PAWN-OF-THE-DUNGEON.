5c9f276239d5cb24ef8c3a132277a4484ffe309f
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-14 18:55:40 +0930

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const config = {
-  type: Phaser.AUTO,
-  width: 1280,
-  height: 720,
-  backgroundColor: '#000000',
-  physics: {
-    default: 'arcade',
-    arcade: { gravity: { y: 0 }, debug: false }
-  },
-  scene: [BootScene, CharacterSelectScene, GameScene, UIScene]
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    backgroundColor: "#000000",
+    physics: {
+        default: "arcade",
+        arcade: { debug: false }
+    },
+    scene: [BootScene, CharacterSelectScene, GameScene, UIScene]
 };
 
 new Phaser.Game(config);
