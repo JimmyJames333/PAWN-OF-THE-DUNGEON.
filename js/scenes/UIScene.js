6c897832e5a9d6fd48e25d9ad119cc35b66a2198
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-14 18:56:57 +0930

    Update UIScene.js

--- js/scenes/UIScene.js
+++ js/scenes/UIScene.js
@@ -1,12 +1,12 @@
 class UIScene extends Phaser.Scene {
-  constructor() {
-    super('UIScene');
-  }
+    constructor() {
+        super("UIScene");
+    }
 
-  create() {
-    this.add.text(20, 20, 'UI Layer Active', {
-      fontSize: '20px',
-      color: '#ffffff'
-    });
-  }
+    create() {
+        this.add.text(20, 20, "Pawn of the Dungeon", {
+            fontSize: "32px",
+            color: "#ffffff"
+        });
+    }
 }
