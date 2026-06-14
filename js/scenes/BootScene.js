5c57f17dcbf1f72f84fe16806c05634da50a075c
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-14 09:58:51 +0930

    Change scene transition from GameScene to CharacterSelectScene

--- js/scenes/BootScene.js
+++ js/scenes/BootScene.js
@@ -4,18 +4,21 @@ class BootScene extends Phaser.Scene {
   }
 
   preload() {
+    // Optional logo, comment out if you don't have it
+    // this.load.image('logo', 'assets/sprites/logo.png');
   }
 
   create() {
     const { width, height } = this.scale;
+
     this.add.text(width / 2, height / 2, 'Loading...', {
       fontSize: '32px',
       color: '#ffffff'
     }).setOrigin(0.5);
 
     this.time.delayedCall(1000, () => {
-      console.log('BootScene finished loading, switching to GameScene...');
-      this.scene.start('GameScene');
+      console.log('BootScene finished loading, switching to CharacterSelectScene...');
+      this.scene.start('CharacterSelectScene');
     });
   }
 }
