c4a03d57b50c0c0b958c2d8029e63a2943bd0b27
JimmyJames333 <qiuxxjx0031@outlook.com>
2026-06-14 10:12:07 +0930

    Update GameScene.js

--- js/scenes/GameScene.js
+++ js/scenes/GameScene.js
@@ -10,8 +10,16 @@ class GameScene extends Phaser.Scene {
     this.map = new MapGenerator(this);
     this.map.createMap();
 
-    this.add.text(width / 2, height / 2, `You chose: ${chosen}`, {
-      fontSize: '32px',
+    // Show a colored circle for the chosen character
+    const colorMap = {
+      Warrior: 0xff4444,
+      Mage: 0x44aaff,
+      Rogue: 0x44ff44
+    };
+
+    this.add.circle(width / 2, height / 2, 60, colorMap[chosen]);
+    this.add.text(width / 2, height / 2 + 120, `You chose: ${chosen}`, {
+      fontSize: '28px',
       color: '#00ff00'
     }).setOrigin(0.5);
   }
